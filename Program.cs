650402748381f4ed37d8c55225887af1b28602d4
Alexandr <manaev-2012@mail.ru>
2014-07-13 20:54:42 +0500

    Zadanie2

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace практика_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Введите значение а");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if (a > 0 & b > 0 & c > 0 && a + b > c && a + c > b && c + b > a)
+                if (a *a + b *b==c*c||a *a+ c * c== b * b|| c * c + b * b == a * a)
+                    Console.WriteLine("треугольник прямоугольный");
+                else
+                    if (a * a + b * b > c * c && a * a + c * c > b * b && c * c + b * b > a * a)
+                        Console.WriteLine("треугольник остроугольный");
+                    else
+                        Console.WriteLine("треугольник тупоугольный");
+            else
+                Console.WriteLine("треугольник не существует");
+            Console.ReadKey();
+
+        }
+    }
+}
